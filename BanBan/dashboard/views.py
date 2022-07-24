0259e1a67c08b262e7f370f10b174dca37771825
leeeeyz77 <102798337+leeeeyz77@users.noreply.github.com>
2022-07-24 20:09:44 +0900

    로그인/게시판

---
+++ BanBan/dashboard/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import PostForm, CommentForm
+from .models import Post 
+
+def home(request):
+    # Posts = Post.objects.all()
+    posts = Post.objects.filter().order_by('-date') 
+    return render(request, 'index.html', {'posts':posts})
+
+def postcreate(request):
+    if request.method == 'POST' or request.method == 'FILES':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')    
+    else: 
+        form = PostForm()
+    return render(request, 'post_form.html',{'form':form})
+
+def detail(request, post_id):
+    post_detail = get_object_or_404(post, pk=post_id)
+    comment_form = CommentForm()
+    return render(request, 'detail.html', {'post_detail':post_detail})
+
+#댓글 저장
+def new_comment(request, post_id):
+    filled_form = CommentForm(request.POST)
+    if filled_form.is_valid():
+        finished_form = filed_form.save(commit=False)
+        finished_form.post = get_object_or_404(post, pk=post_id)
+        finished_form.save()
+
+    return redirect('detail', post_id)
+
