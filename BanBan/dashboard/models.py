4f18ac0db82d1ff4593a127ce1d2b2efb49bb505
leeeeyz77 <102798337+leeeeyz77@users.noreply.github.com>
2022-07-29 02:43:09 +0900

    model

--- BanBan/dashboard/models.py
+++ BanBan/dashboard/models.py
@@ -7,8 +7,10 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    where = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-
+    writeDate = models.DateTimeField(blank=True, null=True)
+ 
     def __str__(self):
         return self.title
 
