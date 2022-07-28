9259ca53019a8c0ecdb0fab2c8fc02a839b7e5b0
hwldus <89963270+hwldus@users.noreply.github.com>
2022-07-28 23:20:35 +0900

    login구현중

--- BanBan/accounts/views.py
+++ BanBan/accounts/views.py
@@ -1,8 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect 
 from django.contrib import auth 
 from django.contrib.auth.models import User
 
 def login(request):
+    # POST 요청이 들어오면 로그인 처리를 해줌
     if request.method == 'POST':
         userid = request.POST['username']
         pwd = request.POST['password']
@@ -12,8 +14,10 @@ def login(request):
             auth.login(request, user)
             return redirect('home')
         else:
-            return render(request, 'bad_login.html') #로그인실패시
-
+            return HttpResponse('로그인 실패. 다시 시도')
+            #return render(request, 'login.html') #로그인실패시
+    
+    # GET 요청이 들어오면 login form을 담고 있는 login.html을 띄워줌
     else:
         return render(request, 'login.html')
 
