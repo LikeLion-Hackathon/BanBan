a38882f91c0bd12ca58cf383e02a5c7faa47a82f
hwldus <89963270+hwldus@users.noreply.github.com>
2022-07-28 12:32:09 +0900

    login수정중

--- BanBan/accounts/views.py
+++ BanBan/accounts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect 
 from django.contrib import auth 
+from django.contrib.auth.models import User
 
 def login(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = auth.authenticate(request, username=username, passsword=password)
+    if request.method == 'POST':
+        userid = request.POST['username']
+        pwd = request.POST['password']
+        user = auth.authenticate(request, username=userid, passsword=pwd)
 
         if user is not None:
             auth.login(request, user)
